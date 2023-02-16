ef92eec587e65eceb69803edc7d5119e69c0d86a
Renato Afonso de Medeiros <renato_ads@outlook.com>
2023-02-16 16:55:48 -0300

    Adicionar arquivos de projeto.

---
+++ POO_16.02/Pessoa.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO_16._02
+{
+    class Pessoa
+    {
+
+        public double peso, altura;
+
+
+
+
+        public double calculo ()
+        {
+            return (peso / (altura * altura));
+
+        }
+
+        public string situacao (double IMC)
+        {
+            string retorno;
+            
+            if ( IMC < 18)
+            {
+                retorno = " Abaixo do Peso ";
+            }
+            else if ( IMC < 25)
+            {
+                retorno = " Peso normal ";
+            }
+
+            else if (IMC < 30)
+            {
+                retorno = " acima do peso ";
+            }
+            else if (IMC < 35)
+            {
+                retorno = " obesidade I ";
+            }
+            else if (IMC < 40)
+            {
+                retorno = " obesidade LL ";
+            }
+            else  
+            {
+                retorno = " obesidade III ";
+            }
+
+            return retorno;
+
+        }
+
+        public void mensagem()
+        {
+            double ObterCalculo = calculo();
+            string ObterSituaçao = situacao( ObterCalculo );
+
+            Console.WriteLine("Seu IMC é de: " + ObterCalculo );
+            Console.WriteLine("Sua situação é de: " + ObterSituaçao);
+
+        }
+        
+
+    }
+}
---
+++ POO_16.02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace POO_16._02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Caluclo IMC");
+
+            Pessoa p = new Pessoa();
+            p.altura = 1.68;
+            p.peso = 70;
+            p.mensagem();
+
+            Console.ReadKey();
+
+
+
+
+        }
+
+
+
+    }
+}
