ca62635e2455f8c2e68b774e05e7f059180bbb59
Renato Afonso de Medeiros <123197747+Renato-AMedeiros@users.noreply.github.com>
2023-02-27 11:22:03 -0300

    Estudo POO 23/02

---
+++ Pessoa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstudoPOO_23_02
+{
+    public class Pessoa
+    {
+        public string nome, cidade, email;
+        public int idade;
+        public double peso, altura, contato;
+        public void Mensagem() {
+             Console.WriteLine("Meu nome é " + nome + ", moro em " + cidade + ", tenho " + idade + " anos, meu peso é " + peso + "Kg, minha altura é "
+                + altura + ", meu email é " + email + " e meu contato é " + contato );
+      
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EstudoPOO_23_02;
+
+Console.WriteLine("Hello, World!");
+
+Pessoa cadastro = new Pessoa();
+
+cadastro.nome = "Renato";
+cadastro.email = "renato_e20@hotmail.com";
+cadastro.altura = 1.68;
+cadastro.cidade = "Suzano";
+cadastro.contato = 11953809793;
+cadastro.peso = 82;
+cadastro.idade = 26;
+cadastro.Mensagem();
+
